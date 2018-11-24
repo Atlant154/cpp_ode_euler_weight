98b2dccc15d93443827369b3a649bc25aace7fde
Patsakula Nikita <nicman2222@gmail.com>
2018-11-24 15:11:16 +0700

    All methods implemented. Test configuration added.

--- src/appr_scheme.cpp
+++ src/appr_scheme.cpp
@@ -1,6 +1,7 @@
 #include "appr_scheme.h"
 
 #include <cmath>
+#include <fstream>
 
 appr_scheme::appr_scheme(unsigned h_num, double (*heat_sources)(double)) :
       h_num_(h_num)
@@ -13,11 +14,6 @@ std::string appr_scheme::get_scheme_name() const {
     return name;
 }
 
-std::vector<std::pair<double, double>> appr_scheme::get_result(std::vector<double> bounds) {
-    return result;
-}
-
-
 double appr_scheme::get_error(double (*exact_solution)(double)) const {
     double error = 0.0;
 
@@ -26,33 +22,117 @@ double appr_scheme::get_error(double (*exact_solution)(double)) const {
     for(unsigned long iter = 0; iter < result.size(); ++iter)
     {
         appr = result[iter].second;
-        exact = exact_solution(left_bound_ + iter * static_cast<double>(h_));
-        error += pow(appr - exact, 2);
+        exact = exact_solution(left_bound_ + static_cast<double>(iter) * h_);
+        error = pow(appr - exact, 2);
     }
     return sqrt(error * h_);
 }
 
-std::vector<std::pair<double, double>> euler_scheme::get_result(std::vector<double> bounds) {
+void appr_scheme::write_to_file() const {
+
+    std::fstream result_file;
+
+    std::string filename;
+
+    filename = "./" + get_scheme_name();
+
+    filename += "Splits: " + std::to_string(h_num_) + ".";
+
+    result_file.open(filename, std::ios::out | std::ios::trunc);
+
+    for (const auto &item : result)
+        result_file << item.first << '\t' << item.second << '\n';
+
+    result_file.close();
+}
+
+void appr_scheme::clear_result_vector() {
+    result.clear();
+}
+
+std::vector<std::pair<double, double>> euler_scheme::get_result(double bound_value) {
 
     if(!result.empty())
         return result;
 
     result.reserve(h_num_ + 1);
 
-    double x, u;
+    result.emplace_back(std::make_pair(left_bound_, bound_value));
 
-    result.emplace_back(std::make_pair(left_bound_, bounds.front()));
+    double x, u;
 
     for(unsigned int iter = 1; iter <= h_num_; ++iter)
     {
         x = left_bound_ + iter * h_;
         u = result.back().second + h_ * heat_sources_(result.back().first);
-        result.emplace_back(std::make_pair(x, u));
+        result.emplace_back(x, u);
     }
     return result;
 }
 
 std::string euler_scheme::get_scheme_name() const{
     std::string name = "Euler method.";
+    return name;
+}
+
+std::vector<std::pair<double, double>> runge_kutta_schene::get_result(double bound_value) {
+
+    if(!result.empty())
+        return result;
+
+    result.reserve(h_num_ + 1);
+
+    result.emplace_back(left_bound_, bound_value);
+
+    double k1, k2, k3, k4;
+
+    double x, last_x, u;
+
+    for(unsigned iter = 1; iter <= h_num_; ++iter)
+    {
+        last_x = result.back().first;
+        k1 = h_ * last_x;
+        k2 = k3 = h_ * (last_x + h_ / 2.0);
+        k4 = h_ * (last_x + h_);
+        x  = left_bound_ + iter * h_;
+        u  = result.back().second + (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
+        result.emplace_back(x, u);
+    }
+
+    return result;
+}
+
+std::string runge_kutta_schene::get_scheme_name() const {
+    std::string name = "Runge-Kutta method.";
+    return name;
+}
+
+std::vector<std::pair<double, double>> weight_scheme::get_result(double bound_1, double bound_2, double sigma) {
+
+    if(!result.empty())
+        return result;
+
+    result.reserve(h_num_ + 1);
+
+    result.emplace_back(left_bound_, bound_1);
+
+    result.emplace_back(left_bound_ + h_, bound_2);
+
+    double x, u;
+
+    for(unsigned iter = 2; iter <= h_num_; ++iter)
+    {
+        x  = left_bound_ + iter * h_;
+        u  = result.back().second * ((2 - 2 * sigma - 4 * h_) / (2 - sigma));
+        u += result[iter - 2].second * sigma / (2 - sigma);
+        u += 2 * h_ * heat_sources_(result.back().first) / (2 - sigma);
+        result.emplace_back(x, u);
+    }
+
+    return result;
+}
+
+std::string weight_scheme::get_scheme_name() const {
+    std::string name = "Weight scheme.";
     return name;
 }
--- src/appr_scheme.h
+++ src/appr_scheme.h
@@ -8,36 +8,62 @@
 class appr_scheme {
 public:
     appr_scheme() = delete;
+
     virtual ~appr_scheme() = default;
 
-    appr_scheme(unsigned h_num, double (*heat_sources)(double));
-
-    virtual std::string get_scheme_name() const;
-
-    virtual  std::vector<std::pair<double, double>> get_result(std::vector<double> bounds);
+    virtual std::string  get_scheme_name() const;
 
     double get_error(double (*exact_solution)(double)) const;
 
+    void clear_result_vector();
+
+    void write_to_file() const;
+
 protected:
+    appr_scheme(unsigned h_num, double (*heat_sources)(double));
+
     std::vector<std::pair<double, double>> result;
     const double left_bound_ = 0.0;
     const double right_bound_ = 1.0;
 
     unsigned h_num_;
+
     double (*heat_sources_)(double);
+
     double h_;
 };
 
 class euler_scheme : public appr_scheme {
 public:
     euler_scheme(unsigned h_num, double (*heat_sources)(double)) : appr_scheme(h_num, heat_sources) {};
+
     ~euler_scheme() override = default;
-    std::vector<std::pair<double, double>> get_result(std::vector<double> bounds) override;
+
+    std::vector<std::pair<double, double>> get_result(double boud_value);
+
     std::string get_scheme_name() const override;
 };
 
 class runge_kutta_schene : public appr_scheme {
+public:
+    runge_kutta_schene(unsigned h_num, double (*heat_sources)(double)) : appr_scheme(h_num, heat_sources) {};
 
+    ~runge_kutta_schene() override = default;
+
+    std::vector<std::pair<double, double>> get_result(double bound_value);
+
+    std::string get_scheme_name() const override;
+};
+
+class weight_scheme : public appr_scheme {
+public:
+    weight_scheme(unsigned h_num, double (*heat_sources)(double)) : appr_scheme(h_num, heat_sources) {};
+
+    ~weight_scheme() override = default;
+
+    std::vector<std::pair<double, double>> get_result(double bound_1, double bound_2, double sigma);
+
+    std::string get_scheme_name() const override;
 };
 
 
