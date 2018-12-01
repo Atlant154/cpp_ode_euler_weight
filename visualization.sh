5749914897bc4217c96fb539f2e897b0f96350be
Patsakula Nikita <nicman2222@gmail.com>
2018-12-01 19:33:49 +0700

    Revert "Tests and benchmarks."

--- visualization.sh
+++ visualization.sh
@@ -6,8 +6,8 @@ gnuplot -persist <<- EOF
     set title "Euler method."
     set xlabel "X"
     set ylabel "Y"
-    FILES = system("ls -1 'Euler_method'.*.txt")
-    LABEL = system("ls -1 'Euler_method'.*.txt | sed -e 's/Euler_method.//' -e 's/.txt//'")
+    FILES = system("ls -1 euler_result_*.txt")
+    LABEL = system("ls -1 euler_result_*.txt | sed -e 's/euler_result_//' -e 's/.txt//'")
 
     plot for [i=1:words(FILES)] word(FILES,i) u 1:2 title word(LABEL,i)  with lines
 EOF
@@ -30,8 +30,8 @@ gnuplot -persist <<- EOF
     set title "Runge-Kutta method."
     set xlabel "X"
     set ylabel "Y"
-    FILES = system("ls -1 Runge-Kutta_method.*.txt")
-    LABEL = system("ls -1 Runge-Kutta_method.*.txt | sed -e 's/Runge-Kutta_method.//' -e 's/.txt//'")
+    FILES = system("ls -1 runge_kutta_*.txt")
+    LABEL = system("ls -1 runge_kutta_*.txt | sed -e 's/runge_kutta_//' -e 's/.txt//'")
 
     plot for [i=1:words(FILES)] word(FILES,i) u 1:2 title word(LABEL,i)  with lines
 EOF
